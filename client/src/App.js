b39201dbd27a3ff09a7a3ba49f54095ee2878aad
sampriti <sampriti026@gmail.com>
2024-10-19 15:43:51 +0530

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SchedulePosts from './SchedulePosts';
-import Dashboard from '../client/src/Dashboard';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
